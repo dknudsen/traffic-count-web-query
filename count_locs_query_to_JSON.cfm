6fac4290e082b25e702c189e83ab8844aececefe
dknudsen <dknudsen@users.noreply.github.com>
2015-04-28 10:52:42 -0400

    most controls activated
    Most tabular controls kick off database query on change, and some update
    their visibility, values, or control list based upon the query response.
    However, no grouping or aggregating behavior yet. Basic map also added
    to provide extent parameters (no representation of count locations,
    selected or otherwise, yet, and no selection via the map).  Bare
    beginnings of d3 manipulation of query response to represent count parts
    to user.

--- count_locs_query_to_JSON.cfm
+++ count_locs_query_to_JSON.cfm
@@ -8,25 +8,25 @@
 <cfset isNoParamInvoke = StructCount(params) EQ 0>
 <!--- set empty values for all undefined parameters to simplify later tests --->
 <cfloop index="p" delimiters="," 
-	list="tn,rt,st,stX,rg,fc,ft,loc,ext,type,catSum,dirSum,lnSum,adt,frm,to,rec,days,sun,mon,tue,wed,thu,fri,sat,mos,j,f,mar,apr,may,jun,jul,aug,s,o,n,d,avg,avgD,avgM,aggr,prj">
+	list="tn,rt,st,stX,rg,fc,ft,loc,ext,minx, maxx, miny, maxy, type,catSum,dirSum,lnSum,adt,frm,to,rec,days,sun,mon,tue,wed,thu,fri,sat,mos,j,f,mar,apr,may,jun,jul,aug,s,o,n,d,avg,avgD,avgM,aggr,prj,init">
 	<cfif NOT IsDefined("params." & p)><cfset params[p] = ""></cfif>
 </cfloop>
 <!--- decide the tables to be joined --->
 <cfset isJoinRoads = (params.fc != "") OR (params.ft != "")>
-<cfset isJoinCountParts = (params.catSum != "") OR (params.dirSum != "") OR (params.lnSum != "") OR (params.adt != "")>
+<cfset isJoinCountParts = (params.catSum != "") OR (params.dirSum != "") OR (params.lnSum != "") OR (params.adt != "" OR True)>
 <cfset isJoinCounts = isJoinCountParts OR (params.type != "") OR (params.frm != "") OR (params.to != "") OR (params.rec != "") OR (params.prj != "")>
 <cfquery name="countLocData" datasource="counts"><cfoutput>
   SELECT 
   	count_location_id, 
-	streetname, 
+	l.streetname, 
     auto_rte_number, 
     alt_rte_number, 
-    roadinventory_id, 
-    l.description, 
+    l.roadinventory_id, 
+    l.description AS loc_desc, 
     l.town_id, 
     town,
-    sde.st_x(shape) AS x,
-    sde.st_y(shape) AS y<cfif isJoinCounts>,
+    sde.st_x(l.shape) AS x,
+    sde.st_y(l.shape) AS y<cfif isJoinCounts>,
     date_first,
     date_last,
     types.type,
@@ -41,7 +41,7 @@
     data_table,
     date_start,
     date_end,
-    description(</cfif>
+    cp.description AS cp_desc</cfif>
   FROM count_locations l, 
     towns_r_data t<cfif isJoinRoads>,
     mpodata.eot_roadinventory r</cfif><cfif isJoinCounts>,
@@ -53,20 +53,21 @@
     AND c.type_id = types.type_id
     AND c.project_id = projects.project_id</cfif><cfif isJoinCountParts>
     AND c.count_id = cp.count_id</cfif>
-    AND town = <cfif params.tn EQ "">'WAKEFIELD'<cfelse><cfoutput>'#params.tn#'</cfoutput></cfif><cfif params.rt NEQ "">
+    AND town = <cfif params.tn EQ "">'WAKEFIELD'<cfelse><cfoutput>'#UCase(params.tn)#'</cfoutput></cfif><cfif params.rt NEQ "">
     AND auto_rte_number = '#params.rt#'</cfif><cfif params.st NEQ "">
-	<cfif params.stX EQ "">AND streetname = '#params.st#'<cfelse>AND SOUNDEX(streetname) = SOUNDEX('#params.st#')</cfif></cfif><cfif params.fc NEQ "">
+	<cfif params.stX EQ "">AND l.streetname = UPPER('#params.st#')<cfelse>AND SOUNDEX(l.streetname) = SOUNDEX('#params.st#')</cfif></cfif><cfif params.fc NEQ "">
     AND r.functionalclassification = #params.fc#</cfif><cfif params.ft NEQ "">
     AND r.facilitytype = '#params.ft#'</cfif><cfif params.loc NEQ "">
-    AND count_location_id IN (#params.loc#)</cfif><cfif params.ext NEQ "">
-    AND x BETWEEN params.minX AND params.maxX AND y BETWEEN params.minY AND params.maxY</cfif><cfif params.type NEQ "">
-    AND types.type = '#params.type#'</cfif><cfif params.frm NEQ "">
-    AND <cfif isJoinCountParts>date_end<cfelse>date_last</cfif> > TO_DATE('#params.frm#')</cfif><cfif params.to NEQ "">
-    AND <cfif isJoinCountParts>date_start<cfelse>date_first</cfif> < TO_DATE('#params.to#')</cfif><cfif params.prj NEQ "">
+    AND count_location_id IN (#params.loc#)</cfif><cfif params.ext NEQ "" AND params.minX NEQ "" AND params.maxX NEQ "" AND params.minY NEQ "" AND params.maxY NEQ "">
+    AND sde.st_overlaps(l.shape, sde.st_polyfromtext('polygon ((#params.minX# #params.minY#, #params.maxX# #params.minY#, #params.maxX# #params.maxY#, #params.minX# #params.maxY#, #params.minX# #params.minY#))', 19)) = 1</cfif><cfif params.type NEQ "">
+    AND types.description = '#params.type#'</cfif><cfif params.adt NEQ "">
+	AND data_table = '<cfif params.adt EQ "a">spanning<cfelse>monthly</cfif>'</cfif><cfif params.frm NEQ "">
+    AND <cfif isJoinCountParts>date_end<cfelse>date_last</cfif> > TO_DATE('#params.frm#','MM/DD/YYYY')</cfif><cfif params.to NEQ "">
+    AND <cfif isJoinCountParts>date_start<cfelse>date_first</cfif> < TO_DATE('#params.to#','MM/DD/YYYY')</cfif><cfif params.prj NEQ "">
     AND projects.name = '#params.prj#'</cfif>
 </cfoutput></cfquery>
 <!--- Look-up table queries --->
-<cfif isNoParamInvoke>
+<cfif isNoParamInvoke OR (params.init NEQ "")>
 	<!--- TYPES --->
 	<cfquery name="typeList" datasource="counts">SELECT * FROM types ORDER BY type</cfquery>
     <!--- PROJECTS --->
@@ -86,6 +87,29 @@
 <cfelse>
 	<cfquery name="dateRange" datasource="counts">SELECT min(date_first) AS min_date, max(date_last) AS max_date FROM counts</cfquery>
 </cfif>
-<cfoutput>{"townList":#SerializeJSON(townList)#,"locationExtent":#SerializeJSON(locationExtent)#,"dateRange":#SerializeJSON(dateRange)#,"data":#SerializeJSON(countLocData)#<cfif
-  isNoParamInvoke>,"typeList":#SerializeJSON(typeList)#,"projectList":#SerializeJSON(projectList)#</cfif>}</cfoutput>
+<!--- DISTINCT TYPES --->
+<!--- DISTINCT CATEGORIES, DIRECTIONS, LANES, DATA_TABLES --->
+<cfif isJoinCounts>
+    <cfquery name="distinctTypes" dbtype="query">SELECT type from countLocData GROUP BY type</cfquery>
+</cfif>
+<cfif isJoinCountParts>
+	<cfquery name="distinctCats" dbtype="query">SELECT category_code FROM countLocData GROUP BY category_code</cfquery>
+    <cfquery name="distinctDirs" dbtype="query">SELECT dir FROM countLocData GROUP BY dir</cfquery>
+    <cfquery name="distinctLanes" dbtype="query">SELECT lanes from countLocData GROUP BY lanes</cfquery>
+    <cfquery name="distinctTables" dbtype="query">SELECT data_table from countLocData GROUP BY data_table</cfquery>
+</cfif>
+
+<!--- OUTPUT the JSON --->
+<cfoutput>{"townList":[</cfoutput><cfoutput query="townList">"#town#",</cfoutput><cfoutput 
+>""],"locationExtent":#SerializeJSON(locationExtent)#,"dateRange":#SerializeJSON(dateRange)#,"data":#SerializeJSON(countLocData)#</cfoutput><cfif 
+isNoParamInvoke OR (params.init NEQ "")><cfoutput
+  >,"typeList":[</cfoutput><cfoutput query="typeList">"#description#",</cfoutput><cfoutput 
+  >""],"projectList":[</cfoutput><cfoutput query="projectList">"#name#",</cfoutput><cfoutput
+  >""]</cfoutput></cfif><cfif 
+isJoinCountParts><cfoutput
+  >,"numCats":#distinctCats.RecordCount#,"numDirs":#distinctDirs.RecordCount#,"numLanes":#distinctLanes.RecordCount#</cfoutput><cfoutput 
+  query="distinctTables">,"data_#data_table#":true</cfoutput></cfif><cfif 
+isJoinCounts><cfoutput 
+  query="distinctTypes"><cfif distinctTypes.RecordCount EQ 1>,"distinctType":"#type#"</cfif></cfoutput></cfif><cfoutput
+>}</cfoutput>
 </cfprocessingdirective>
--- counts.js
+++ counts.js
@@ -3,15 +3,135 @@ var CTPS = {};
 CTPS.countsApp = {};
 
 CTPS.countsApp.initSubmit = function() {
-	$('#fromDateControl').datepicker({ dateFormat: "mm/dd/yy", numberOfMonths: 2 });
-	$('#fromDateControl').change(CTPS.countsApp.queryOnControlChange);
-	$('#toDateControl').datepicker({ dateFormat: "mm/dd/yy", numberOfMonths: 2 });
+	// init the map control
+	CTPS.countsApp.project = proj4('PROJCS["NAD83 / Massachusetts Mainland",GEOGCS["NAD83",DATUM["North_American_Datum_1983",SPHEROID["GRS 1980",6378137,298.257222101,AUTHORITY["EPSG","7019"]],AUTHORITY["EPSG","6269"]],PRIMEM["Greenwich",0,AUTHORITY["EPSG","8901"]],UNIT["degree",0.01745329251994328,AUTHORITY["EPSG","9122"]],AUTHORITY["EPSG","4269"]],UNIT["metre",1,AUTHORITY["EPSG","9001"]],PROJECTION["Lambert_Conformal_Conic_2SP"],PARAMETER["standard_parallel_1",42.68333333333333],PARAMETER["standard_parallel_2",41.71666666666667],PARAMETER["latitude_of_origin",41],PARAMETER["central_meridian",-71.5],PARAMETER["false_easting",200000],PARAMETER["false_northing",750000],AUTHORITY["EPSG","26986"],AXIS["X",EAST],AXIS["Y",NORTH]]');
+	CTPS.countsApp.map = L.map('queryMapControl').setView([42.359,-71.06],11);
+	L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png',
+				{
+					attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors',
+					maxZoom: 18
+				}).addTo(CTPS.countsApp.map);
+	CTPS.countsApp.map.on('moveend', function(e) {
+											  var bounds = e.target.getBounds();
+											  var ll = CTPS.countsApp.project.forward([bounds.getWest(),bounds.getSouth()]);
+											  var ur = CTPS.countsApp.project.forward([bounds.getEast(),bounds.getNorth()]);
+											  $('#minXControl').val(ll[0]);
+											  $('#maxXControl').val(ur[0]);
+											  $('#minYControl').val(ll[1]);
+											  $('#maxYControl').val(ur[1]);
+											  CTPS.countsApp.queryOnControlChange();
+											  });
+	
+	// init the tabular controls
+	$('#townControl').autocomplete();
+	$('#routeControl').on('change', CTPS.countsApp.queryOnControlChange);
+	$('#streetControl').on('change', CTPS.countsApp.queryOnControlChange);
+	$('#streetExactControl').on('change', CTPS.countsApp.queryOnControlChange);
+	$('#regionControl').on('change', CTPS.countsApp.queryOnControlChange);
+	$('#funcClassControl').on('change', CTPS.countsApp.queryOnControlChange);
+	$('#facTypeControl').on('change', CTPS.countsApp.queryOnControlChange);
+	$('#locIDControl').on('change', CTPS.countsApp.queryOnControlChange);
+	$('#withinMapControl').on('change', CTPS.countsApp.queryOnControlChange);
+	$('#typeControl').autocomplete({"minLength": 0, "delay": 0, "change": CTPS.countsApp.queryOnControlChange});
+	$('#sumCatsControl').on('change', CTPS.countsApp.queryOnControlChange);
+	$('#sumDirsControl').on('change', CTPS.countsApp.queryOnControlChange);
+	$('#sumLanesControl').on('change', CTPS.countsApp.queryOnControlChange);
+	$('#ADTAnnualControl').on('change', CTPS.countsApp.queryOnControlChange);
+	$('#ADTMonthlyControl').on('change', CTPS.countsApp.queryOnControlChange);
+	$('#fromDateControl').datepicker({"dateFormat": "mm/dd/yy", "numberOfMonths": 1, "changeMonth": true, "changeYear": true, "constrainInput": true,
+									 "onSelect": CTPS.countsApp.queryOnControlChange});
+	$('#toDateControl').datepicker({"dateFormat": "mm/dd/yy", "numberOfMonths": 1, "changeMonth": true, "changeYear": true, "constrainInput": true,
+								   "onSelect": CTPS.countsApp.queryOnControlChange});
+	$('#allDaysControl').on('change', function(e) {
+											   if (e.target.checked) {
+												   $('#daysDiv input[type=checkbox]').prop('checked',false);
+												   $('#daysDiv').hide();
+											   } else $('#daysDiv').show();
+											   CTPS.countsApp.queryOnControlChange();
+											   });
+	$('#daysDiv input[type=checkbox]').on('change', function(e) {
+															 if ($('#daysDiv :checked').length == 7) {
+															     $('#daysDiv input[type=checkbox]').prop('checked',false);
+																 $('#allDaysControl').prop('checked',true);
+																 $('#daysDiv').hide();
+															 }
+															 CTPS.countsApp.queryOnControlChange();
+															 });
+	$('#allMonthsControl').on('change', function(e) {
+												 if (e.target.checked) {
+													 $('#monthsDiv input[type=checkbox]').prop('checked',false);
+													 $('#monthsDiv').hide();
+												 } else $('#monthsDiv').show();
+												 CTPS.countsApp.queryOnControlChange();
+												 });
+	$('#monthsDiv input[type=checkbox]').on('change', function(e) {
+															   if ($('#monthsDiv :checked').length == 12) {
+																   $('#monthsDiv input[type=checkbox]').prop('checked',false);
+																   $('#allMonthsControl').prop('checked',true);
+																   $('#monthsDiv').hide();
+															   }
+															   CTPS.countsApp.queryOnControlChange();
+															   });
+	$('#hourlyControl').on('change', CTPS.countsApp.queryOnControlChange);
+	$('#qtrHourlyControl').on('change', CTPS.countsApp.queryOnControlChange);
+	$('#projControl').autocomplete({"minLength": 0, "delay": 0, "change": CTPS.countsApp.queryOnControlChange});
+	
+	$('#theForm').append('<input id="initControl" name="init" value="y" type="hidden">');
+	CTPS.countsApp.queryOnControlChange();
+	$('#initControl').remove();
 }; // CTPS.countsApp.init()
 
 CTPS.countsApp.queryOnControlChange = function() {
 	$.getJSON("count_locs_query_to_JSON.cfm",
 			  $('#theForm').serialize(),
 			  function (data) {
-				  console.log(data);
+				  CTPS.countsApp.data = data;
+				  if (typeof(data.townList) !== 'undefined') $('#townControl').autocomplete("option", "source", data.townList);
+				  if (typeof(data.typeList) !== 'undefined') $('#typeControl').autocomplete("option", "source", data.typeList);
+				  if (typeof(data.numCats) !== 'undefined' && data.numCats > 1) {
+					  $('#sumCatsDiv').show();
+				  } else {
+					  $('#sumCatsDiv').hide();
+				  }
+				  if (typeof(data.numDirs) !== 'undefined' && data.numDirs > 1) {
+					  $('#sumDirsDiv').show();
+			  	  } else {
+					  $('#sumDirsDiv').hide();
+				  }
+				  if (typeof(data.numLanes) !== 'undefined' && data.numLanes > 1) {
+					  $('#sumLanesDiv').show();
+				  } else {
+					  $('#sumLanesDiv').hide();
+				  }
+				  if (typeof(data.distinctType) !== 'undefined' && data.distinctType === 'ADT') {
+					  $('#ADTDiv').show();
+					  if (document.forms['theForm'].elements['adt'].value === '') $('#ADTAnnualControl').prop('checked',true);
+				  } else {
+					  $('#ADTDiv').hide();
+					  $('#ADTAnnualControl').prop('checked',false);
+					  $('#ADTMonthlyControl').prop('checked',false);
+				  }
+				  if (typeof(data.dateRange) !== 'undefined') {
+					  $('#fromDateControl').val(data.dateRange.DATA.length > 0 ? $.datepicker.formatDate('mm/dd/yy',new Date(data.dateRange.DATA[0][0])) : '');
+					  $('#toDateControl').val(data.dateRange.DATA.length > 0 ? $.datepicker.formatDate('mm/dd/yy',new Date(data.dateRange.DATA[0][1])) : '');
+				  }
+				  if (typeof(data.data_quarter_hourly) !== 'undefined') {
+					  $('#aggregationIntervalDiv').show();
+					  if (document.forms['theForm'].elements['aggr'].value === '') $('#hourlyControl').prop('checked',true);
+				  } else {
+					  $('#aggregationIntervalDiv').hide();
+					  $('#hourlyControl').prop('checked',false);
+					  $('#qtrHourlyControl').prop('checked',false);
+				  }
+				  if (typeof(data.projectList) !== 'undefined') $('#projControl').autocomplete("option", "source", data.projectList);
+				  CTPS.countsApp.nest = d3.nest()
+				  		.key(function(d) { return d[0] }).sortKeys(d3.ascending)
+						.key(function(d) { return d[12] }).sortKeys(d3.ascending)
+						.key(function(d) { return d[20] }).sortKeys(d3.ascending)
+						.key(function(d) { return d[16] }).sortKeys(d3.ascending)
+						.key(function(d) { return d[21] }).sortKeys(d3.ascending)
+						.key(function(d) { return d[19] }).sortKeys(d3.ascending)
+						.entries(CTPS.countsApp.data.data.DATA)
+				  CTPS.countsApp.treeNodes = d3.layout.tree().children(function(d) { return d.values }).nodes(CTPS.countsApp.nest);
 			  });
 }; // CTPS.countsApp.queryOnControlChange()
